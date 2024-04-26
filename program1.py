14ecadc3f321b23c75dc2880c55d0231114facdc
Yashpal Jat <124427715+Yashpal-jat@users.noreply.github.com>
2024-04-26 13:32:09 +0530

    Add files via upload

--- program1.py
+++ program1.py
@@ -1,21 +1,17 @@
-def smallest_missing_positive_integer(nums: List[int]) -> int:
+def smallest_missing_positive_integer(nums: list[int]) -> int:
     """
     Implement the function smallest_missing_positive_integer 
     using the provided smallest_missing_positive_integer function 
     to find the smallest missing positive integer in the given list.
 
     """
-    pass
-
-
-
-
-
-
 
     
 
-
+    nums=set(nums)
+    for i in range(1,2147483647):
+        if i not in nums:      
+            return i
 
   
 
--- program2.py
+++ program2.py
@@ -7,7 +7,23 @@ def longest_substring(s: str) -> int:
     the length of the longest substring without repeating characters.
 
     """ 
-    pass
 
+
+    n = len(s)
+    maxLength = 0
+    charSet = set()
+    left = 0
+        
+    for right in range(n):
+        if s[right] not in charSet:
+            charSet.add(s[right])
+            maxLength = max(maxLength, right - left + 1)
+        else:
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            charSet.add(s[right])
+        
+    return maxLength
 
 
